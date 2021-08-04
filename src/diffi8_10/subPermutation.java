208c2fd2ed9ac548175df31e36bf314d0553425e
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-04 23:17:49 +0530

    Update subPermutation.java

--- src/diffi8_10/subPermutation.java
+++ src/diffi8_10/subPermutation.java
@@ -1,9 +1,10 @@
-package diffi8_10;
+//package diffi8_10;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class subPermutation {
-    
+    static StringBuilder sb=new StringBuilder();
     public static void main(String ar[]){
         Scanner in=new Scanner(System.in);
 
@@ -14,14 +15,17 @@ public class subPermutation {
             in.nextLine();
             String s=in.nextLine();
             int count=0;
-            for(int i=0;i<n-1;i++){
-                char firstvl=s.charAt(i);
-                char secon=s.charAt(i+1);
 
-                int comp=Character.compare(firstvl,secon);
-                if(comp>0)  count++;
+            char[] arr=s.toCharArray();
+            Arrays.sort(arr);
+            String s2=String.valueOf(arr);
+
+            for(int i=0;i<n;i++){
+                int com=Character.compare(s.charAt(i),s2.charAt(i))
+                if(com!=0) count++;
             }
             sb.append(count+"\n");
         }
+        System.out.println(sb.toString());
     }
 }
