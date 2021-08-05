dde9c93c3cef60958f5854e9c63550a31a86b8c4
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-05 22:59:30 +0530

    Create wonderColor.java

---
+++ src/diffi8_10/wonderColor.java
@@ -0,0 +1,33 @@
+//package diffi8_10;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class wonderColor {
+    static StringBuilder sb=new StringBuilder();
+    public static void main(String ar[]){
+        Scanner in=new Scanner(System.in);
+
+        int t=in.nextInt();
+        in.nextLine();
+        while(t-->0){
+            String [] s=in.nextLine().split("");
+            if(s.length<2) sb.append("0\n");
+
+            else{
+                HashMap<String,Integer> hs=new HashMap<>();
+
+                for(int i=0;i<s.length;i++){
+                    if(hs.containsKey(s[i]))    hs.put(s[i],2);
+                    else hs.put(s[i],1);
+                }
+                long sum=0;
+                for(int i=0;i<s.length;i++){
+                    sum+=hs.get(s[i]);
+                }
+                sb.append(sum/2+"\n");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
