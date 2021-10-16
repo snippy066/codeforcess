ace79cc74ab178adfc7a5c4f7cbb5ed04fffbddf
Priyanka Thakur <priyankathakur221206@gmail.com>
2021-10-16 16:06:53 +0530

    Create prob50A.java

---
+++ src/diffi8_10/prob50A.java
@@ -0,0 +1,13 @@
+package diffi8_10;
+
+import java.util.Scanner;
+
+public class prob50A {
+    public static void main(String ar[]){
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        
+    }
+}
